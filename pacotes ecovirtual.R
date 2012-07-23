a3f21769a0ddd6fdd99c9c70cb0ec8ebe373c25c
melinatarituba <melinatarituba@yahoo.com.br>
2012-07-23 19:39:37 +0000

    colocando arquivos

---
+++ pacotes ecovirtual.R
@@ -0,0 +1,14 @@
+#ecovirtual melina
+
+#instalando o rcmdr
+install.packages("Rcmdr")
+## para iniciar o Rcmdr:
+library(Rcmdr)
+
+#pacotes do ecovirtual baixado no site da disciplina
+
+install.packages("ecovirtual_1.0.tar.gz" ,repos=NULL)  
+
+install.packages("rcmdrplugin.ecovirtual_1.0.tar.gz" ,repos=NULL) # VAIO new
+
+testemelina
