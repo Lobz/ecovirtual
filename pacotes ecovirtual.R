1a0a0de0b2e0040a9aaf86c05e9d5bc9c1768bb1
melinatarituba <melinatarituba@yahoo.com.br>
2014-02-12 20:25:59 +0000

    check dos pacotes

--- pacotes ecovirtual.R
+++ pacotes ecovirtual.R
@@ -7,8 +7,8 @@ R CMD check **endereço da pasta**
 #instalando o rcmdr
 install.packages("Rcmdr")
 #pacotes do ecovirtual
-install.packages("/home/ale/Documentos/Ale2013/AleProjetos/EcoVirtual/EcoVirtual_0.05.tar.gz" ,repos=NULL)  
-install.packages("/home/ale/Documentos/Ale2013/AleProjetos/EcoVirtual/RcmdrPlugin.EcoVirtual_0.05.tar.gz" ,repos=NULL) # VAIO new
+install.packages("/home/ecologia/EcoVirtual_0.05.tar.gz" ,repos=NULL)  
+install.packages("/home/ecologia/RcmdrPlugin.EcoVirtual_0.05.tar.gz",repos=NULL) # VAIO new
 ## para iniciar o Rcmdr:
 library(Rcmdr)
 
