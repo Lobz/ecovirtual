1e53ad0f648db1301ec9558456cfc95d0356068d
melinatarituba <melinatarituba@yahoo.com.br>
2014-02-12 15:40:17 +0000

    organizacao pacote

--- pacotes ecovirtual.R
+++ pacotes ecovirtual.R
@@ -1,6 +1,9 @@
 #ecovirtual - montando pacotes pelo terminal
 R CMD build /home/ale/Documentos/Ale2013/AleProjetos/EcoVirtual_svn/EcoVirtual
 R CMD build /home/ale/Documentos/Ale2013/AleProjetos/EcoVirtual_svn/RcmdrPlugin.EcoVirtual
+#ecovirtual - checando os pacotes:
+R CMD check **endereço da pasta**
+
 #instalando o rcmdr
 install.packages("Rcmdr")
 #pacotes do ecovirtual
